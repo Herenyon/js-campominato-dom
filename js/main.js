45b13fa35684daff3ed17f252fed608b6d052eee
Herenyon <rikitur17@gmail.com>
2023-03-05 16:28:11 +0100

    creazione griglia base finita

--- js/main.js
+++ js/main.js
@@ -3,68 +3,33 @@
 // funzioni
 
 
-function myCreateElement(htmlElement, coloreCelleBlu, coloreCelleRosso, cellNumber, modalita, mode, htmlValue) {
+function myCreateElement(htmlElement, mode, htmlValue) {
     const element = document.createElement(htmlElement);
     // element.classList.add(coloreCelleRosso);
-    element.classList.add(mode);
+    // element.classList.add(mode);
     element.innerText = htmlValue;
     // element.classList.add(coloreCelleBlu);
-
-    if (modalita === 'easyMode') {
+    if (mode === 'easyMode') {
+        
 
         element.classList.add('celle-10');
     }
 
-    else if (modalita === 'midMode') {
-
+    else if (mode === 'midMode') {
+        
         element.classList.add('celle-8');
     }
 
-    else if (modalita === 'hardMode') {
-
+    else if (mode === 'hardMode') {
+        
         element.classList.add('celle-7');
     }
-    
-   
-const bombe = randomNumber(cellNumber);
-        console.log(bombe);
-    
-
-    element.addEventListener('click', function () {
-        
-        for (let i = 0; i < cellNumber; i++)
-            if (bombe[i] === cellNumber){
-                element.classList.add (coloreCelleRosso);
-                location.reload();
-            }
-            else {
-
-                element.classList.add (coloreCelleBlu);
-            }
-
-        console.log(htmlValue);
 
 
-    }
-    )
+
+
     return element;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -76,56 +41,60 @@ function myAppendElement(containerElement, htmlElement) {
 
 
 
-function createGrid() {
-
-    const selectMode = document.getElementById('mode');
-    const selectModeValue = selectMode.value
-    let mode;
-    let cellNumber;
-    let classeCelle;
-    // let bombe;
-
-    if (selectModeValue === 'easyMode') {
-
-        
-        mode = selectModeValue;
-        cellNumber = 100;
-
-
-
-    }
-    else if (selectModeValue === 'midMode') {
-        mode = selectModeValue;
-        cellNumber = 81;
-        
-
-    }
-
-    else {
-
-        mode = selectModeValue;
-        cellNumber = 49;
-        
-
-    }
-
-    console.log(mode);
-    console.log(cellNumber);
-    // const bombe = randomNumber(cellNumber);
-
-    // console.log(bombe);
-    
-
+function createGrid(cellNumber, griglia, numeroBombe) {
     for (let i = 1; i <= cellNumber; i++) {
 
         console.log(i)
-        const createElement = myCreateElement('div', 'colore-celle-blu', 'colore-celle-rosso', cellNumber, mode, classeCelle, i);
+        const createElement = myCreateElement('div',griglia, i);
         myAppendElement(containerBoard, createElement);
 
     }
+}
+
+
+// NON TOCCARE
+function grandezzaGriglia(selectModeValue) {
+
+    let mode;
+    if (selectModeValue === 'easyMode') {
+        mode = selectModeValue;
+
+    }
+
+    else if (selectModeValue === 'midMode') {
+        mode = selectModeValue;
+        
+    }
+
+    else if (selectModeValue === 'hardMode') {
+        mode = selectModeValue;
+
+    }
+    return mode;
 
 }
 
+// NON TOCCARE
+function numeroCelle(selectModeValue) {
+
+    let cellNumber;
+
+    if (selectModeValue === 'easyMode') {       
+        cellNumber = 100;
+    }
+    else if (selectModeValue === 'midMode') {        
+        cellNumber = 81;
+    }
+
+    else {   
+        cellNumber = 49;
+    }
+
+    return cellNumber;
+
+}
+
+// NON TOCCARE
 function randomNumber(cellNumber) {
 
     let bombe = [];
@@ -159,20 +128,59 @@ let buttonValue = false;
 
 
 
+
+
+
+
+
+
+
+// element.addEventListener('click', function () {
+
+//     for (let i = 0; i < cellNumber; i++)
+//         if (bombe[i] === cellNumber) {
+//             element.classList.add(coloreCelleRosso);
+//             location.reload();
+//         }
+//         else {
+
+//             element.classList.add(coloreCelleBlu);
+//         }
+
+//     console.log(htmlValue);
+
+
+// }
+// )
+
+
+
 playButton.addEventListener('click',
-    function () {
-        if (buttonValue) {
-            location.reload();
-            buttonValue = false;
+    
+function () {
+    const selectMode = document.getElementById('mode');
+    const selectModeValue = selectMode.value;
 
-        }
-        else {
 
-            createGrid();
-            buttonValue = true;
-            document.getElementById('play').value = "Rispristina";
+    const cellNumber = numeroCelle(selectModeValue);
+    console.log(cellNumber);
+    const griglia = grandezzaGriglia(selectModeValue);
+    console.log(griglia);
+    const bombe = randomNumber(cellNumber);
+    console.log(bombe);
+    
+    if (buttonValue) {
+        location.reload();
+        buttonValue = false;
 
-        }
     }
+    else {
+
+        createGrid(cellNumber, griglia, bombe);
+        buttonValue = true;
+        document.getElementById('play').value = "Rispristina";
+
+    }
+}
 
 )
